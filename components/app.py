2771542b9836ae061543520e62533b9891a394ba
Richard Bramley <rabramley@gmail.com>
2016-10-25 11:21:52 +0100

    Yamlise

--- components/app.py
+++ components/app.py
@@ -1,53 +1,25 @@
 #!/usr/bin/env python
 
+import yaml
 from weasyprint import HTML
 from jinja2 import Environment, FileSystemLoader
+from shutil import copyfile
 
 env = Environment(loader=FileSystemLoader('.'))
 template = env.get_template("template.html")
 
-players = [
-	{
-		'first_name': 'Mike',
-		'last_name' : 'Cruncher',
-		'position' : 'Defence',
-		'flavour' : 'Mike is a hunky defender that has broken as many heart as he has ankles.'
-	},
-	{
-		'first_name': 'Tony',
-		'last_name' : 'Cleaver',
-		'position' : 'Defence',
-		'flavour' : 'Tony doesn\'t think much, and then only in single syllables.'
-	},
-	{
-		'first_name': 'Dave',
-		'last_name' : 'Mallet',
-		'position' : 'Defence',
-		'flavour' : 'Dave loves smashing bones almost as much as he loves his old mum.'
-	},
-	{
-		'first_name': 'Archibald',
-		'last_name' : 'Talbot',
-		'position' : 'Defence',
-		'flavour' : 'Archie is as crafty as they come.'
-	},
-	{
-		'first_name': 'Terence',
-		'last_name' : 'Swiftfoot',
-		'position' : 'Attack',
-		'flavour' : 'You could call Terry quick, but he won\'t hear as he\'ll already be 40 yards away.'
-	},
-	{
-		'first_name': 'Ethelbert',
-		'last_name' : 'Skillton',
-		'position' : 'Attack',
-		'flavour' : 'Elthelbert is a practitioner of the beautiful game.'
-	}
-]
+with open("players.yaml", 'r') as players_yaml:
+	players = yaml.load(players_yaml)
 
 template_vars = {"title" : "This is a test",
                  "players": players}
 
 html = template.render(template_vars)
+
+text_file = open("/components_pdfs/players.html", "w")
+text_file.write(html)
+text_file.close()
+
+copyfile('./players.css', '/components_pdfs/players.css')
 
 HTML(string=html, base_url='.').write_pdf('/components_pdfs/players.pdf')
