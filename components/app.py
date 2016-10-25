3f2879b929d41a485acd141eaab0400ca8bdb1fa
Richard Bramley <rabramley@gmail.com>
2016-10-25 14:19:04 +0100

    Refactor

--- components/app.py
+++ components/app.py
@@ -5,22 +5,25 @@ from weasyprint import HTML
 from jinja2 import Environment, FileSystemLoader
 from shutil import copyfile
 
-env = Environment(loader=FileSystemLoader('.'))
-template = env.get_template("players.html")
+def produce_cards(card_type):
+    env = Environment(loader=FileSystemLoader('.'))
+    template = env.get_template('{0}.html'.format(card_type))
 
-with open("players.yaml", 'r') as players_yaml:
-	players = yaml.load(players_yaml)
+    with open('{0}.yaml'.format(card_type), 'r') as players_yaml:
+        cards = yaml.load(players_yaml)
 
-template_vars = {"title" : "This is a test",
-                 "players": players}
+    template_vars = {"cards": cards}
 
-html = template.render(template_vars)
+    html = template.render(template_vars)
 
-text_file = open("/components_pdfs/players.html", "w")
-text_file.write(html)
-text_file.close()
+    text_file = open('/components_pdfs/{0}.html'.format(card_type), "w")
+    text_file.write(html)
+    text_file.close()
+
+    HTML(string=html, base_url='.').write_pdf('/components_pdfs/{0}.pdf'.format(card_type))
+
+produce_cards('players')
 
 copyfile('./main.css', '/components_pdfs/main.css')
 copyfile('./players.css', '/components_pdfs/players.css')
 
-HTML(string=html, base_url='.').write_pdf('/components_pdfs/players.pdf')
